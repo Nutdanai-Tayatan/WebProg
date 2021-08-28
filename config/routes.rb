97ed1e8d2e83b8759b24d8ece0bc6850b86eda56
Nutdanai-Tayatan <60133733+Nutdanai-Tayatan@users.noreply.github.com>
2021-08-28 18:53:27 +0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get 'main/contact'
   get 'main/welcome'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "welcome", to: "main#welcome"
+  get "contact_us" , to: "main#contact"
 end
