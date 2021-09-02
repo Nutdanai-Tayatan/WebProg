e7ccd5a05c84eed0d0d05e484cc778fedf1c735f
Nutdanai <nud.thy@gmail.com>
2021-09-02 09:13:19 +0700

    ex2 second

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,22 @@
 class MainController < ApplicationController
-  def contact
+  def contact_us
   end
 
   def welcome
   end
+
+  def info
+    a = params[:a]
+    b = params[:b]
+    @start = a.to_i
+    @stop = b.to_i
+  end
+
+  def loop
+    a = params[:a]
+    b = params[:b]
+    @start = a.to_i
+    @stop = b.to_i
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
-  get 'main/contact'
-  get 'main/welcome'
+
+  get "main/welcome"
+  get "main/contact_us"
+
+   
+  get 'main/info'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+<<<<<<< HEAD
   get "welcome", to: "main#welcome"
   get "contact_us" , to: "main#contact"
+=======
+
+  get 'contact_us', to: 'main#contact'
+  get 'welcome', to: 'main#welcome'
+
+  get 'main/loop'
+
+>>>>>>> exercise 2
 end
